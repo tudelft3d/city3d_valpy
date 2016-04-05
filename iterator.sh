358c3072319d3d901ab6e69a1dcfc7f8c1aeec72
Du Xin <dxin@godzilla.bk.tudelft.nl>
2016-04-05 19:02:40 +0200

    update iterator.sh

--- iterator.sh
+++ iterator.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# currentpath=$(cd `dirname $0`; pwd)
-# outputfile=$currentpath/result/$2
-# #echo $outputfile
-# /Users/octeufer/Work/val3dity/val3dity $1 --oxml $outputfile
-# filename=${outputfile##*/}
-# #echo $filename
-# output=`echo "$filename"|awk -F'.' '{printf("%s.semantic.%s.%s",$1,$3,$4)}'`
-# #echo $output
-# python semantic_check.py $1 $3 $outputfile > $currentpath/result/$output
-
-for file in `ls $1|grep '\($2\)'`
+for file in `ls $1|grep '\(building\)'`
 do
-	fname=`echo ${file:0:${#file}-4}`
+	echo $file
+	fname=`echo ${file:0:${#file}}`
 	echo $fname
 done
