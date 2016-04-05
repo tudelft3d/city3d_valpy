f44fdfeee4c6cd42ac1bd6a879550ec01140280d
Du Xin <octeufer@gmal.com>
2016-04-05 19:24:24 +0200

    fix iterator

--- iterator.sh
+++ iterator.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [! -d "$3"];then
+	mkdir "$3"
+fi
 for file in `ls $1|grep '\(building\)'`
 do
-	echo $file
-	fname=`echo ${file:0:${#file}}`
-	echo $fname
+	#echo $file
+	fname=`echo ${file:0:${#file}-4}`
+	#echo $fname
+	fpath=$1'/'$file
+	echo $3/fname
+	#sh valid_order.sh fpath $3/fname.geo.report.xml 3
 done
