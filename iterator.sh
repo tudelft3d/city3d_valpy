5bd72ee1b1ddb508bed56551e6c3027843d73af3
Du Xin <octeufer@gmal.com>
2016-04-05 18:43:24 +0200

    add iterator

---
+++ iterator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+currentpath=$(cd `dirname $0`; pwd)
+outputfile=$currentpath/result/$2
+#echo $outputfile
+/Users/octeufer/Work/val3dity/val3dity $1 --oxml $outputfile
+filename=${outputfile##*/}
+#echo $filename
+output=`echo "$filename"|awk -F'.' '{printf("%s.semantic.%s.%s",$1,$3,$4)}'`
+#echo $output
+python semantic_check.py $1 $3 $outputfile > $currentpath/result/$output
+
+for file in `ls $1|grep '\($2\)'`
+do
+	fname=`echo ${file:0:${#file}-4}`
+	echo $fname
+done
